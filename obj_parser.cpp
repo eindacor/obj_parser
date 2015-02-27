a41670095795a2f73bf503a9fbac12b32c06c191
Joseph Pollack <jpollack320@gmail.com>
2015-02-26 19:49:15 -0500

    parser working

--- obj_parser.cpp
+++ obj_parser.cpp
@@ -1,2 +1,282 @@
 #include "obj_parser.h"
 
+void vertex_data::setData()
+{
+	for (vector<float>::const_iterator it = v_data.begin(); it != v_data.end(); it++)
+		face_data.push_back(*it);
+
+	for (vector<float>::const_iterator it = vt_data.begin(); it != vt_data.end(); it++)
+		face_data.push_back(*it);
+
+	for (vector<float>::const_iterator it = vn_data.begin(); it != vn_data.end(); it++)
+		face_data.push_back(*it);
+}
+
+obj_contents::obj_contents(const char* obj_file)
+{
+	fstream file;
+	file.open(obj_file, std::ifstream::in);
+
+	int position_counter = 1;
+	int uv_counter = 1;
+	int normal_counter = 1;
+	int paramspace_counter = 1;
+	vertex_count = 0;
+	face_count = 0;
+
+	while (!file.eof())
+	{
+		OBJ_DATA_TYPE type = NOT_SET;
+
+		string line;
+		std::getline(file, line, '\n');
+
+		switch (line[0])
+		{
+		case 'v':
+			if (line[1] == ' ')
+				type = POSITION;
+
+			if (line[1] == 't')
+				type = UV;
+
+			if (line[1] == 'n')
+				type = NORMAL;
+
+			if (line[1] == 'p')
+				type = PARAM_SPACE;
+
+			break;
+
+		case 'f': type = FACE_DEF; break;
+		case '#': continue;
+		case '\n': continue;
+		default: break;
+		}
+
+		if (type != FACE_DEF && type != NOT_SET)
+		{
+			vector<float> floats(extractFloats(line));
+
+			switch (type)
+			{
+			case POSITION:
+				v_data[position_counter] = floats;
+				position_counter++;
+				break;
+			case UV:
+				vt_data[uv_counter] = floats;
+				uv_counter++;
+				break;
+			case NORMAL:
+				vn_data[normal_counter] = floats;
+				normal_counter++;
+				break;
+			case PARAM_SPACE:
+				vp_data[paramspace_counter] = floats;
+				paramspace_counter++;
+				break;
+			default: break;
+			}
+		}
+
+		else if (type == FACE_DEF)
+		{
+			//face_data contains the index list for each line (each face)
+			//	1/1/1  2/2/2  3/3/3
+			vector< vector<int> > extracted_face_data(extractFaceSequence(line));
+		
+			vector<vertex_data> face;
+			//generates positions, uv coordinates, and normals for the line (each sequence line = 1 face)
+			for (vector< vector<int> >::iterator it = extracted_face_data.begin(); it != extracted_face_data.end(); it++)
+			{
+				int v_index = (*it)[0];
+				int vt_index = (*it)[1];
+				int vn_index = (*it)[2];
+				vector<float> position_data = v_data.at(v_index);
+				vector<float> uv_data = vt_data.at(vt_index);
+				vector<float> normal_data = vn_data.at(vn_index);
+
+				vertex_data vert(position_data, uv_data, normal_data);
+				vertex_count++;
+				face.push_back(vert);
+
+				all_v_data.insert(all_v_data.end(), position_data.begin(), position_data.end());
+				all_vt_data.insert(all_vt_data.end(), uv_data.begin(), uv_data.end());
+				all_vn_data.insert(all_vn_data.end(), normal_data.begin(), normal_data.end());
+			}
+			faces.push_back(face);
+			face_count++;
+		}
+	}
+
+	setData();
+	file.close();
+}
+
+void obj_contents::setData()
+{
+	for (vector< vector<vertex_data> >::const_iterator faces_it = faces.begin();
+		faces_it != faces.end(); faces_it++)
+	{
+		//object data format will be:
+		//		position.x, position.y, position.z, [position.w],
+		//		uv.x, [uv.y], [uv.w],
+		//		normal.x, normal.y, normal.z,
+		//	bracketed values are only included if they were in the original obj file
+
+		//for each vertex in each face, pass the stored, ordered data to obj_data
+		for (vector<vertex_data>::const_iterator vertex_it = faces_it->begin();
+			vertex_it != faces_it->end(); vertex_it++)
+		{
+			vector<float> all_face_data(vertex_it->getData());
+			interleave_data.insert(interleave_data.end(), all_face_data.begin(), all_face_data.end());
+		}
+	}
+
+	if (faces.begin() != faces.end())
+	{
+		interleave_stride = faces.begin()->begin()->getStride();
+		interleave_vt_offset = faces.begin()->begin()->getUVOffset();
+		interleave_vn_offset = faces.begin()->begin()->getNOffset();
+
+		v_count = faces.begin()->begin()->getVCount();
+		vt_count = faces.begin()->begin()->getVTCount();
+		vn_count = faces.begin()->begin()->getVNCount();
+	}
+}
+
+const vector<float> extractFloats(const string &s)
+{
+	vector<float> floats;
+
+	vector<int> digits;
+	bool negative = false;
+	bool decimal_found = false;
+	int decimal_places = 0;
+	bool values_begin = false;
+
+	for (int i = 0; i < s.size(); i++)
+	{
+		if (s[i] == '-')
+			negative = true;
+
+		else if (s[i] == '.')
+			decimal_found = true;
+
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			int char_int = '0';
+			char_int = s[i] - char_int;
+			digits.push_back(char_int);
+			if (decimal_found)
+				decimal_places++;
+		}
+
+		//if space found or end of string
+		if ((s[i] == ' ' && values_begin) || i == s.size() - 1)
+		{
+			float extracted = 0;
+
+			for (int n = 0; n < digits.size(); n++)
+			{
+				//example--> 470.258
+				//size = 6
+				//decimal_places = 3
+				//first iteration of loop points to 4, which is in the
+				//10^2 location. 
+				//6 - 3 - 1 - 0 = 2 for the first loop
+				//6 - 3 - 1 - 1 = 1 for the second loop
+				int nth = digits.size() - decimal_places - 1 - n;
+				float multiplier = pow(10.0f, float(nth));
+				float toAdd = digits[n] * multiplier;
+				extracted += toAdd;
+			}
+			
+			if (negative)
+				extracted *= -1.0f;
+
+			floats.push_back(extracted);
+
+			//resets counters
+			digits.clear();
+			negative = false;
+			decimal_found = false;
+			decimal_places = 0;
+		}
+
+		else if (s[i] == ' ' && !values_begin)
+			values_begin = true;
+	}
+
+	return floats;
+}
+
+const vector< vector<int> > extractFaceSequence(const string &s)
+{
+	vector< vector<int> > index_list;
+
+	vector<int> digits;
+	vector<int> sequence;
+	bool values_begin = false;
+
+	for (int i = 0; i < s.size(); i++)
+	{
+		if (s[i] == '/')
+		{
+			if (digits.size() == 0)
+			{
+				sequence.push_back(0);
+				continue;
+			}
+
+			float extracted = 0;
+
+			for (int n = 0; n < digits.size(); n++)
+			{
+				int nth = digits.size() - 1 - n;
+				float multiplier = pow(10.0f, float(nth));
+				float toAdd = digits[n] * multiplier;
+				extracted += toAdd;
+			}
+
+			sequence.push_back(int(extracted));
+
+			//resets counters
+			digits.clear();
+		}
+
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			int char_int = '0';
+			char_int = s[i] - char_int;
+			digits.push_back(char_int);
+		}
+
+		//if space found or end of string
+		if ((s[i] == ' ' && values_begin) || i == s.size() - 1)
+		{
+			float extracted = 0;
+
+			for (int n = 0; n < digits.size(); n++)
+			{
+				int nth = digits.size() - 1 - n;
+				float multiplier = pow(10.0f, float(nth));
+				float toAdd = digits[n] * multiplier;
+				extracted += toAdd;
+			}
+
+			sequence.push_back(int(extracted));
+			index_list.push_back(sequence);
+
+			//resets counters
+			sequence.clear();
+			digits.clear();
+		}
+
+		else if (s[i] == ' ' && !values_begin)
+			values_begin = true;
+	}
+
+	return index_list;
+}
--- obj_parser.h
+++ obj_parser.h
@@ -5,137 +5,111 @@
 #include <string>
 #include <fstream>
 #include <map>
-#include <glm.hpp>
+#include <math.h>
+#include <iostream>
 
+using std::string;
 using std::map;
-using glm::vec2;
-using glm::vec3;
-using glm::vec4;
 using std::vector;
+using std::fstream;
+
+enum OBJ_DATA_TYPE { NOT_SET, FACE_DEF, POSITION, UV, NORMAL, PARAM_SPACE };
+
+const vector<float> extractFloats(const string &s);
+const vector< vector<int> > extractFaceSequence(const string &s);
 
 class vertex_data
 {
 public:
-	/*
-	//constructors with vec4 as position
-	vertex_data(const vec4 &p);
-	vertex_data(const vec4 &p, const vec2 &uv);
-	vertex_data(const vec4 &p, const vec3 &n);
-	vertex_data(const vec4 &p, const vec2 &uv, const vec3 &n);
-
-	//constructors with vec3 as position
-	vertex_data(const vec3 &p) : vertex_data(vec4(p, 1.0f)) {};
-	vertex_data(const vec3 &p, const vec2 &uv) : vertex_data(vec4(p, 1.0f), uv) {};
-	vertex_data(const vec3 &p, const vec3 &n) : vertex_data(vec4(p, 1.0f), n) {};
-	vertex_data(const vec3 &p, const vec2 &uv, const vec3 &n) : vertex_data(vec4(p, 1.0f), uv, n) {};
-	*/
 	vertex_data(const vector<float> &p, const vector<float> &uv, const vector<float> &n) :
-		position(p), uv_coords(uv), normals(n) { setData(); }
+		v_data(p), vt_data(uv), vn_data(n) { setData(); }
 
 	~vertex_data(){};
 
-	const int getUVOffset() const { return position.size() * sizeof(float); }
-	const int getNOffset() const { return getUVOffset() + (uv_coords.size() * sizeof(float)); }
-	const int getStride() const { return data.size() * sizeof(float); }
-	const bool containsUV() const { return uv_coords.size(); }
-	const bool containsN() const { return normals.size(); }
+	const int getUVOffset() const { return v_data.size() * sizeof(float); }
+	const int getNOffset() const { return getUVOffset() + (vt_data.size() * sizeof(float)); }
+	const int getStride() const { return face_data.size() * sizeof(float); }
+	const bool containsUV() const { return vt_data.size(); }
+	const bool containsN() const { return vn_data.size(); }
+	const int getVCount() const { return v_data.size(); }
+	const int getVTCount() const { return vt_data.size(); }
+	const int getVNCount() const { return vn_data.size(); }
+	const vector<float> getVData() const { return v_data; }
+	const vector<float> getVTData() const { return vt_data; }
+	const vector<float> getVNData() const { return vn_data; }
+
+	vector<float> getData() const { return face_data; }
 
 private:
 	void setData();
-	vector<float> position;
-	vector<float> uv_coords;
-	vector<float> normals;
+	vector<float> v_data;
+	vector<float> vt_data;
+	vector<float> vn_data;
 
-	vector<float> data;
+	vector<float> face_data;
 };
 
-/*
-vertex_data::vertex_data(const vec4 &p)
-{
-	position.push_back(p.x);
-	position.push_back(p.y);
-	position.push_back(p.z);
-	position.push_back(p.w);
-
-	setData();
-}
-
-vertex_data::vertex_data(const vec4 &p, const vec2 &uv)
-{
-	position.push_back(p.x);
-	position.push_back(p.y);
-	position.push_back(p.z);
-	position.push_back(p.w);
-
-	uv_coords.push_back(uv.x);
-	uv_coords.push_back(uv.y);
-
-	setData();
-}
-
-vertex_data::vertex_data(const vec4 &p, const vec3 &n)
-{
-	position.push_back(p.x);
-	position.push_back(p.y);
-	position.push_back(p.z);
-	position.push_back(p.w);
-
-	normals.push_back(n.x);
-	normals.push_back(n.y);
-	normals.push_back(n.z);
-
-	setData();
-}
-
-vertex_data::vertex_data(const vec4 &p, const vec2 &uv, const vec3 &n)
-{
-	position.push_back(p.x);
-	position.push_back(p.y);
-	position.push_back(p.z);
-	position.push_back(p.w);
-
-	uv_coords.push_back(uv.x);
-	uv_coords.push_back(uv.y);
-
-	normals.push_back(n.x);
-	normals.push_back(n.y);
-	normals.push_back(n.z);
-
-	setData();
-}
-*/
-
-void vertex_data::setData()
-{
-	for (vector<float>::const_iterator it = position.begin(); it != position.end(); it++)
-		data.push_back(*it);
-
-	for (vector<float>::const_iterator it = uv_coords.begin(); it != uv_coords.end(); it++)
-		data.push_back(*it);
-
-	for (vector<float>::const_iterator it = normals.begin(); it != normals.end(); it++)
-		data.push_back(*it);
-}
-
-template<typename v_type, typename vt_type, typename vp_type>
 class obj_contents
 {
 public:
-	obj_contents();
+	obj_contents(const char* obj_file);
 	~obj_contents(){};
 
-	const int getStride() const { return face_data.begin()->size(); }
+	const int getFloatCount() const { return interleave_data.size(); }
+
+	const int getInterleaveStride() const { return interleave_stride; }
+	const int getInterleaveVTOffset() const { return interleave_vt_offset; }
+	const int getInterleaveVNOffset() const { return interleave_vn_offset; }
+	const vector<float> getInterleaveData() const { return interleave_data; }
+
+	//returns # of floats per vertex type
+	const int getVCount() const { return v_count; }
+	const int getVTCount() const { return vt_count; }
+	const int getVNCount() const { return vn_count; }
+	//returns # of vertices stored
+	const int getVertexCount() const { return vertex_count; }
+	//returns # of faces stored
+	const int getFaceCount() const { return face_count; }
+
+	const vector<float> getVData() const { return all_v_data; }
+	const vector<float> getVTData() const { return all_vt_data; }
+	const vector<float> getVNData() const { return all_vn_data; }
+	const vector<float> getVPData() const { return all_vp_data; }
 
 private:
 	void setData();
-	map<int, v_type> v_data;
-	map<int, vec2> vt_data;
-	map<int, vec3> n_data;
-	map<int, vp_type> vp_data;
+
+	//uses vector<float> because # of floats per vertex varies
+	//data direct from obj file, unformatted
+	map<int, vector<float> > v_data;
+	map<int, vector<float> > vt_data;
+	map<int, vector<float> > vn_data;
+	map<int, vector<float> > vp_data;
 
 	//vector of faces, each face is a vector of vertices
-	vector< vector<vertex_data> > face_data;
-	vector<float> boj_data;
+	vector< vector<vertex_data> > faces;
+
+	//contain all vertices for all faces, accessed when
+	//vertices are not to be interleaved
+	vector<float> all_v_data;
+	vector<float> all_vt_data;
+	vector<float> all_vn_data;
+	vector<float> all_vp_data;
+
+	//interleaved data values
+	int interleave_stride;
+	int interleave_vt_offset;
+	int interleave_vn_offset;
+	vector<float> interleave_data;
+
+	//counts floats per vertex of each type (3 = vec3, 4 = vec4)
+	int v_count;
+	int vt_count;
+	int vn_count;
+	//# of vertices are stored total
+	int vertex_count;
+	//# of faces are stored total
+	int face_count;
 };
 
 #endif
